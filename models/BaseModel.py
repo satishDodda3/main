c964208dc95edab022836806b0c0a47054916194
satish dodda <satishdodda@satishs-MacBook-Air.local>
2024-07-01 12:21:57 -0500

    new commit

--- models/BaseModel.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-class BaseModel(ABC):
-    def __init__(self):
-        super().__init__()
-    
-    @abstractmethod
-    def similarity(self,inputs):
-        pass
